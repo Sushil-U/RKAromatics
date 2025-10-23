bf044e04ce79ac94ad81c33008fed75fcb80d480
Sushil-U <sushil.212it032@nitk.edu.in>
2025-10-23 15:11:39 +0530

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,10 +16,10 @@ const Footer = () => {
       <div className="footer-center">
         <h3>Quick Links</h3>
         <ul>
-          <li><a href="#">Home</a></li>
+          <li><a href="\">Home</a></li>
           <li><a href="#">Products</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
+          <li><a href="\about">About</a></li>
+          <li><a href="\contact">Contact</a></li>
         </ul>
       </div>
 
