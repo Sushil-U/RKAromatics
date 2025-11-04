5be25178e1073674640ffc38f7f9d79dda6a8413
Sushil-U <sushil.212it032@nitk.edu.in>
2025-11-04 20:56:32 +0530

    Products updated

--- src/components/TopProducts.jsx
+++ src/components/TopProducts.jsx
@@ -8,28 +8,28 @@ const TopProducts = () => {
 
   const products = [
     {
-      id: 1,
-      name: "Royal Perfume",
-      price: "₹1199",
-      image: "/images/product1.jpg",
+      id: 9,
+      name: "Car and Cabin Diffuser RK-0909",
+      price: "₹4,299",
+      image: "/images/product-id/10009/1.jpg",
     },
     {
-      id: 2, 
-      name: "Blossom Scent",
-      price: "₹999",
-      image: "/images/product2.jpg",
-    },
-    {
-      id: 3,
-      name: "GYPSY Essential",
-      price: "₹899",
-      image: "/images/product3.jpg",
+      id: 3, 
+      name: "Automatic Tower Diffuser FB-2311 (Black)",
+      price: "₹16,999",
+      image: "/images/product-id/10003/1.jpg",
     },
     {
       id: 4,
-      name: "Drip Nation",
-      price: "₹1379",
-      image: "/images/product4.jpg",
+      name: "Smart Fragrance Diffuser BW-2712",
+      price: "₹6,299",
+      image: "/images/product-id/10004/1.jpg",
+    },
+    {
+      id: 6,
+      name: "Automatic Hand Dryer",
+      price: "₹2,899",
+      image: "/images/product-id/10006/1.jpg",
     },
   ];
 
